57c391cba846a54d600384075472223c269a7962
MyGit_wsh <wsh@git.com>
2023-03-10 02:08:33 +0800

    wsh-branch分支的第一次提交

--- src/test/TesteGit.java
+++ src/test/TesteGit.java
@@ -7,5 +7,7 @@ public class TesteGit {
         System.out.println("123");
         System.out.println("456");
         System.out.println("789");
+        System.out.println("hello git");
+
     }
 }
