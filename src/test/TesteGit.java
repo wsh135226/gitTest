cdce9dc542fbe622810411a8c97118a9946f964e
MyGit_wsh <wsh@git.com>
2023-03-09 23:04:31 +0800

    测试idea集成git

--- src/test/TesteGit.java
+++ src/test/TesteGit.java
@@ -5,5 +5,6 @@ public class TesteGit {
     public static void main(String[] args) {
         System.out.println("测试git");
         System.out.println("123");
+        System.out.println("456");
     }
 }
